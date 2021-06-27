87a8d1f26c3c90975e12675d11f910581f6075ca
Kangho Kenneth Yoon <yoonkangho@gmail.com>
2021-06-27 15:41:19 +0900

    initial commit

---
+++ src/main/kotlin/blog/kanghokennethyoon/springbootpractice/SpringbootpracticeApplication.kt
@@ -0,0 +1,11 @@
+package blog.kanghokennethyoon.springbootpractice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringbootpracticeApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringbootpracticeApplication>(*args)
+}
